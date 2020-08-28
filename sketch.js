45d0597a26280b31fc9c78e9034053465424b9f2
dev220106 <68177443+dev220106@users.noreply.github.com>
2020-08-28 19:50:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas;
+var gameState=0;
+var playerCount;
+var database;
+var allPlayers;
+var form,player,game;
+
+function setup(){
+    createCanvas(500,500);
+  
+    database=firebase.database()
+    game=new Game();
+    game.getState();
+    game.start();
+}
+
+
+function draw(){
+  if (playerCount===4){
+      game.update(1);
+
+  }
+  if (gameState===1){
+      clear();
+      game.play();
+  }
+}
